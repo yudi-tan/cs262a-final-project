4faa7bd75bc14e26953774a9ddc12384f49c2c39
Justin <justinwyang@berkeley.edu>
2020-11-02 22:07:31 -0800

    Minor additions to test.py

--- event-sourced-shipping-app/test.py
+++ event-sourced-shipping-app/test.py
@@ -10,7 +10,15 @@ s1.depart.remote("B")
 s1.arrive.remote("I")
 s1.unload.remote("some cargo")
 
-log = s1.getLog.remote()
-log = ray.get(log)
+log = ray.get(s1.getLog.remote())
 
+# Complete replay test
 s2 = Ship.replay(log)
+assert log == ray.get(s2.getLog.remote())
+assert ray.get(s1.getState.remote()) == ray.get(s2.getState.remote())
+
+# Time travel test
+s3 = Ship.replay(log[:2])
+_, s3_location, s3_cargo = ray.get(s3.getState().remote())
+assert log[:2] == ray.get(s2.getLog.remote())
+assert s3_location == "SEA" and s3_cargo == ["some cargo"]
