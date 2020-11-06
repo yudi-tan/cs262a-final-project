c5bfe801055afd49098f4e13e502a145949099db
Justin <justinwyang@berkeley.edu>
2020-11-05 21:47:17 -0800

    Fixed minor bug in old test, removed __pycache__

--- event-sourced-shipping-app/test.py
+++ event-sourced-shipping-app/test.py
@@ -18,7 +18,7 @@ assert log == ray.get(s2.getLog.remote())
 assert ray.get(s1.getState.remote()) == ray.get(s2.getState.remote())
 
 # Time travel test
-s3 = Ship.replay(log[:2])
-_, s3_location, s3_cargo = ray.get(s3.getState().remote())
-assert log[:2] == ray.get(s2.getLog.remote())
+s3 = Ship.replay(log[:3])
+_, s3_location, s3_cargo = ray.get(s3.getState.remote())
+assert log[:3] == ray.get(s3.getLog.remote())
 assert s3_location == "SEA" and s3_cargo == ["some cargo"]
