0bcf192b1df171f1900993e230ef04190c3a07e0
zyc <billyzyc65535@gmail.com>
2020-11-18 11:59:02 -0800

    banking app: barebone done

---
+++ event-sourced-banking-app/barebone/bankingactor.py
@@ -0,0 +1,81 @@
+import command
+import json
+import datetime
+import time
+import os
+
+snapshots = {}
+
+class BankingActor:
+    def __init__(self, snapshot=None):
+        self.ledger = {}
+        self.log = []
+        self.__replay_events(snapshot)
+    
+    def process_command(self, command):
+        if command.command_type == "WITHDRAW":
+            event_dict = json.loads(command.payload)
+            if event_dict["amount"] > self.ledger.get(event_dict["user"], 0):
+                return False
+        elif command.command_type != "DEPOSIT":
+            return False
+        
+        self.log.append((command.command_type, command.payload, datetime.date.today().strftime("%Y-%m-%d %H:%M:%S")))
+        self.__process_event(command.command_type, command.payload)
+        return True
+    
+    def snapshot(self):
+        offset = len(self.log)
+        snapshot_dict = {}
+        snapshot_dict["data"] = self.ledger
+        snapshot_dict["offset"] = offset # placeholder
+        snapshot_dict["timestamp"] = datetime.date.today().strftime("%d-%m-%Y %H:%M:%S")
+        snapshot = json.dumps(snapshot_dict, indent = 4)
+
+        snapshots[offset] = snapshot
+    
+    def retrieve_balance(self, user):
+        return self.ledger[user]
+    
+    def retrieve_ledger(self):
+        return self.ledger
+
+    def moving_min_deposit(self, start_time, end_time):
+        q = filter(lambda e: e[2] >= start_time and e[2] <= end_time, self.log)
+        result = float('inf')
+        for event in q:
+            if event[0] == "DEPOSIT":
+                event_dict = json.loads(event[1])
+                amount = event_dict["amount"]
+                result = min(result, amount)
+        if result == float('inf'):
+            return 0
+        return result
+    
+    def moving_sum_deposit(self, start_time, end_time):
+        q = filter(lambda e: e[2] >= start_time and e[2] <= end_time, self.log)
+        total = 0
+        for event in q:
+            if event[0] == "DEPOSIT":
+                event_dict = json.loads(event[1])
+                amount = event_dict["amount"]
+                total += amount
+        return total
+    
+    def __replay_events(self, snapshot):
+        offset = 0
+        if snapshot:
+            snapshot_dict = json.loads(snapshot)
+            self.ledger = snapshot_dict["data"]
+            offset = snapshot_dict["offset"]
+        for event in self.log[offset:]:
+            self.__process_event(event[0], event[1])
+
+    def __process_event(self, event_type, payload):
+        event_dict = json.loads(payload)
+        user = event_dict["user"]
+        amount = event_dict["amount"]
+        if event_type == "DEPOSIT":
+            self.ledger[user] = self.ledger.get(user, 0) + amount
+        elif event_type == "WITHDRAW":
+            self.ledger[user] -= amount
---
+++ event-sourced-banking-app/barebone/main.py
@@ -0,0 +1,47 @@
+import os
+import shutil
+from command import Command
+import json
+from bankingactor import *
+
+def add_command(type, user, amount):
+    payload_dict = {}
+    payload_dict["user"] = user
+    payload_dict["amount"] = amount
+    payload = json.dumps(payload_dict, indent = 4)
+    command = Command(type, payload)
+    res = a.process_command(command)
+    if not res:
+        print(f"{type} command failed")
+
+if __name__ == "__main__":
+    a = BankingActor()
+    add_command("DEPOSIT", "alice", 100)
+    add_command("WITHDRAW", "alice", 50)
+
+    a.snapshot()
+    alice = a.retrieve_balance("alice")
+    print("Balance: ", alice)
+    res = a.moving_min_deposit('2007-01-01 10:00:00', '2077-01-01 10:00:00')
+    print("Moving min: ", res)
+    res = a.moving_sum_deposit('2007-01-01 10:00:00', '2077-01-01 10:00:00')
+    print("Moving sum: ", res)
+
+    add_command("WITHDRAW", "alice", 25)
+    add_command("DEPOSIT", "bob", 200)
+    # should fail and not get recorded
+    add_command("WITHDRAW", "alice", 100)
+
+    a.snapshot()
+    res = a.retrieve_balance("alice")
+    print("alice Balance: ", res)
+    res = a.retrieve_balance("bob")
+    print("bob Balance: ", res)
+
+    b = BankingActor(snapshots[2])
+
+    print("Recovered from snapshot:")
+    res = a.retrieve_balance("alice")
+    print("alice Balance: ", res)
+    res = a.retrieve_balance("bob")
+    print("bob Balance: ", res)
