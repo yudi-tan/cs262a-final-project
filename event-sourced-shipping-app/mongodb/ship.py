dac2bdbb3da4e62c6b2ccb505dd1adad69daed42
Justin <justinwyang@berkeley.edu>
2020-12-07 07:57:02 -0800

    Implemented lazy querying, including separate offsets for more efficiency

--- event-sourced-shipping-app/mongodb/ship.py
+++ event-sourced-shipping-app/mongodb/ship.py
@@ -17,30 +17,35 @@ class Ship(Actor):
         self.location: str
         self.owner: str
         self.cargo: List[str]
-        self.offset = 0
+
+        self.loc_offset = 0
+        self.owner_offset = 0
+        self.cargo_offset = 0
+
         self.client = pymongo.MongoClient(db)
         self.db = self.client.shipping_app
         self.collection = self.db.ship_logs
-        if replay:
-            path = os.path.join(SNAPSHOTS_DIR, name)
-            if os.path.exists(path):
-                snapshots = os.listdir(path)
-                if snapshots:
-                    with open(os.path.join(path, max(snapshots)), 'r') as f:
-                        snapshot_dict = json.load(f)
-                    self.name = snapshot_dict["name"]
-                    self.location = snapshot_dict["location"]
-                    self.owner = snapshot_dict["owner"]
-                    self.cargo = snapshot_dict["cargo"]
-                    self.offset = snapshot_dict["offset"]
 
-            selected = self.collection.find({'ship': name}).skip(self.offset)
-            for eventDict in selected:
-                event = Ship.eventFromDict(eventDict)
-                self.offset += 1
-                self.eventHandler(event)
-        else:
+        if not replay:
             self.on(Ship.Creation(name, location))
+            return
+
+        path = os.path.join(SNAPSHOTS_DIR, name)
+        if not os.path.exists(path):
+            return
+        snapshots = os.listdir(path)
+        if snapshots:
+            with open(os.path.join(path, max(snapshots)), 'r') as f:
+                snapshot_dict = json.load(f)
+            self.name = snapshot_dict["name"]
+            self.location = snapshot_dict["location"]
+            self.owner = snapshot_dict["owner"]
+            self.cargo = snapshot_dict["cargo"]
+            self.loc_offset = snapshot_dict["loc_offset"]
+            self.owner_offset = snapshot_dict["owner_offset"]
+            self.cargo_offset = snapshot_dict["cargo_offset"]
+
+        self.eventHandler(self.collection.find({'ship': self.name}).next())
 
     # Create a snapshot of the current state
     def snapshot(self):
@@ -49,9 +54,11 @@ class Ship(Actor):
         snapshot_dict["location"] = self.location
         snapshot_dict["owner"] = self.owner
         snapshot_dict["cargo"] = self.cargo
-        snapshot_dict["offset"] = self.offset
+        snapshot_dict["loc_offset"] = self.loc_offset
+        snapshot_dict["owner_offset"] = self.owner_offset
+        snapshot_dict["cargo_offset"] = self.cargo_offset
 
-        pathname = os.path.join(SNAPSHOTS_DIR, self.name, f'{self.offset}.json')
+        pathname = os.path.join(SNAPSHOTS_DIR, self.name, f'{self.loc_offset + self.owner_offset + self.cargo_offset}.json')
         os.makedirs(os.path.dirname(pathname), exist_ok=True)
         with open(pathname, 'w') as f:
             json.dump(snapshot_dict, f, indent = 4)
@@ -78,43 +85,62 @@ class Ship(Actor):
             raise NotImplementedError
 
     def on(self, event: Event):
+        if not isinstance(event, Event):
+            return
         self.collection.insert_one({
             "type": event.__class__.__qualname__,
             **event.to_dict()
         })
-        self.offset += 1
-        self.eventHandler(event)
+        if isinstance(event, Ship.Creation):
+            self.eventHandler(event)
 
     def depart(self, origin: str):
+        self.getLocation()
         if self.location == "SEA" or self.location != origin:
             raise Ship.InvalidActionException
         self.on(Ship.Departure(self.name, origin))
 
     def arrive(self, target: str):
+        self.getLocation()
         if self.location != "SEA":
             raise Ship.InvalidActionException
         self.on(Ship.Arrival(self.name, target))
 
     def load(self, cargo: str):
+        self.getLocation()
+        self.getCargo()
         if self.location == "SEA":
             raise Ship.InvalidActionException
         self.on(Ship.Load(self.name, cargo))
 
     def unload(self, cargo: str):
+        self.getLocation()
+        self.getCargo()
         if self.location == "SEA" or cargo not in self.cargo:
             raise Ship.InvalidActionException
         self.on(Ship.Unload(self.name, cargo))
+
+    def __replay(self, cursor):
+        count = 0
+        for eventDict in cursor:
+            event = Ship.eventFromDict(eventDict)
+            self.eventHandler(event)
+            count += 1
+        return count
 
     def getName(self):
         return self.name
 
     def getLocation(self):
+        self.loc_offset += self.__replay(self.collection.find({'ship': self.name, '$or': [{'type': 'Ship.Departure'}, {'type': 'Ship.Arrival'}]}).skip(self.loc_offset))
         return self.location
 
     def getOwner(self):
+        self.owner_offset += self.__replay(self.collection.find({'ship': self.name, 'type': 'Ship.TransferOwnership'}).skip(self.owner_offset))
         return self.owner
 
     def getCargo(self):
+        self.cargo_offset += self.__replay(self.collection.find({'ship': self.name, '$or': [{'type': 'Ship.Load'}, {'type': 'Ship.Unload'}]}).skip(self.cargo_offset))
         return self.cargo
 
     def getLog(self):
